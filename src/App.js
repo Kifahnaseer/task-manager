515a55e944bc0ee67243a14e12ec44fd8bddf3ea
kifahnaseer <kifahnaeer28@gmail.com>
2023-03-06 14:50:37 +0300

    added neccessary files

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { useEffect, useState } from 'react';
 import Task from './components/Task';
 import Home from './views/Home';
 import NavBar from './components/NavBar';
-
+import Header from './components/header';
 import Login from './components/Login';
 import NewTask from './components/NewTask';
 import TaskDetails from './components/TaskDetails';
-import TaskList from './components/TaskList'
+import TaskList from './components/TaskList';
 
 function App() {
 
@@ -16,8 +16,8 @@ function App() {
   const navigate = useNavigate()
 useEffect(() => {
 
-  const id_of_user = localStorage.getItem('id_of_user')
-  id_of_user?navigate("/"):navigate("/login")
+  const user_id = localStorage.getItem('user_id')
+  user_id?navigate("/"):navigate("/login")
 }, [])
 
 console.log(user)
@@ -36,6 +36,8 @@ console.log(user)
         <Route exact path = '/taskdetails' element = {<TaskDetails />} />
 
         <Route exact path = '/tasklist' element = {<TaskList />} />
+
+        <Route exact path = 'header' element = {<Header/>} />
 
       </Routes>
     </div>
